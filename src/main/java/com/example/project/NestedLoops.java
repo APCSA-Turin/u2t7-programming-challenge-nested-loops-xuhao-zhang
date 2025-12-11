c4fbf2ff88e9e2d7173768e5e9c3c79d9ca7547b
Xuhao Zhang <xuhaoz@nycstudents.net>
2025-12-11 03:24:08 +0000

    A meaningful commit message describing your updates

--- src/main/java/com/example/project/NestedLoops.java
+++ src/main/java/com/example/project/NestedLoops.java
@@ -17,19 +17,34 @@ return the string below for given height=5
 *****  
            */
 public static String starStaircase(int height){
-    return "";
+    String toBeReturnedString = "";
+    for (int j = 1; j <= height; j++) {
+        for (int i = 1; i <= height; i++) {
+            if (i <= j) {
+               toBeReturnedString += "*";
+            }
+        }
+    if (j != height){
+        toBeReturnedString += "\n";
+    }
+    }
+    return toBeReturnedString;
 }
-          /*
-return the string below for given height=5
-*****
-****
-***
-**
-*
-           */
+
 
 public static String starStaircaseReverse(int height){
-    return "";
+    String toBeReturnedString = "";
+    for (int j = 1; j <= height; j++) {
+        for (int i = 1; i <= height; i++) {
+            if (i >= j) {
+               toBeReturnedString += "*";
+            }
+        }
+    if (j != height) {
+    toBeReturnedString += "\n";
+    }
+    }
+    return toBeReturnedString;
 }
             /*
 
@@ -41,8 +56,29 @@ width=5 height=5
 *****
           */
     public static String emptyBox(int width, int height) {
+    String toBeReturned = "";
+    width--;
+    height--;
+    if (height + 1 == 1 | width + 1 == 1) {
         return "";
     }
+    for (int i = 0; i <= height; i++) //everything repeat as many times as height (5)
+    {
+        for (int o = 0; o <= width; o++) {
+            if ((o == 0 | o == width) && (i != 0 && i != height)) {
+                toBeReturned += "*";
+            }
+            if (i == height | i == 0) {
+                toBeReturned += "*";
+            } else if (i < height && i > 0 && o > 0 && o < width) {
+                toBeReturned += " ";
+            }
+        }
+        if (i != height)
+        toBeReturned += "\n";
+    }
+    return toBeReturned;
+}
                     /*
 return the string below for word="HELLO" rows=5                    
 HELLO
@@ -54,7 +90,18 @@ OHELL
   i will provide a hint for this one if you get stuck.. use modulo
                 */
     public static String repeatRectangle(String word, int rows){
-        return "";
+        String wordChanged = word;
+        String returnValue = "";
+        for (int i = 1; i <= rows; i++) 
+        {
+            returnValue += wordChanged;
+            if (i != rows) {
+                returnValue += "\n";
+            }
+            wordChanged = wordChanged.substring(1, wordChanged.length()) + wordChanged.substring(0, 1);
+        }
+
+        return returnValue;
     }
 
 }
--- src/main/java/com/example/project/NestedLoopsRunner.java
+++ src/main/java/com/example/project/NestedLoopsRunner.java
@@ -2,7 +2,8 @@ package com.example.project;
 
 public class NestedLoopsRunner {
     public static void main(String[] args) {
-     
+        System.out.println(NestedLoops.emptyBox(1, 5));
+        //System.out.println(NestedLoops.starStaircaseReverse());
     }
 }
 
